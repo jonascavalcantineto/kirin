eb02e0111696770cd87c23dfebf8ae48151c0fe3
ifyes <ifyes@funcap-08798.funceme.br>
2017-04-17 16:22:24 -0300

    Kirin first methods

--- kirin.py
+++ kirin.py
@@ -1,5 +1,25 @@
 # -*- coding: utf-8 -*-
 class Kirin:
-  def __init__(self,fromAddr,toAddr):
-  		self.fromAddr = fromAddr
-  		self.toAddr = toAddr
+    server = ""
+    con_port = ""
+    from_addr = ""
+    to_addr = ""
+    subject = ""
+    msg = ""
+
+    def __init__(self,server, con_port):
+  		self.server = server
+  		self.con_port = con_port
+
+    def setFrom(from_addr):
+		self.from_addr = from_addr
+
+    def setTo(to_addr):
+		self.to_addr = to_addr
+
+    def setSubject(subject):
+    	self.subject = subject
+
+    def setMessage(msg):
+        self.msg = msg
+        
